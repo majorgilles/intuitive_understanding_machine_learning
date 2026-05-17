04a084dc60fa763eb4e240e61955e1c309f9aed4
Gilles Major <major.gilles@gmail.com>
2026-05-17 20:32:44 +0200

    Add optimizer for weight and bias updates in line learner

--- weeks/week-02-pytorch-basics/torch_line_learner.py
+++ weeks/week-02-pytorch-basics/torch_line_learner.py
@@ -1,16 +1,16 @@
 import torch
-from numpy import dtype
 
 from mlcourse.device import get_device
+
+LEARNING_RATE = 0.01
+
 
 def main() -> None:
     device = get_device()
     print(f"Using device: {device}")
 
     x = torch.tensor(
-        [-5, -4, -3, -2 , -1, 0, 1, 2, 3, 4, 5],
-        dtype=torch.float32,
-        device=device
+        [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5], dtype=torch.float32, device=device
     )
     y = 2 * x + 1
     print(f"x:{x}")
@@ -18,6 +18,7 @@ def main() -> None:
 
     weight = torch.tensor(0.0, dtype=torch.float32, device=device, requires_grad=True)
     bias = torch.tensor(0.0, dtype=torch.float32, device=device, requires_grad=True)
+    optimizer = torch.optim.SGD([weight, bias], LEARNING_RATE)
 
     predictions = weight * x + bias
     errors = predictions - y
@@ -31,6 +32,10 @@ def main() -> None:
     loss.backward()
     print("Weight gradient:", weight.grad)
     print("Bias gradient:", bias.grad)
+
+    optimizer.step()
+    print("Updated weight:", weight)  # weight goes up
+    print("Updated bias:", bias)  # bias goes up
 
 
 if __name__ == "__main__":
